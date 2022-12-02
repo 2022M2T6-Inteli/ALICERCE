2dcb7be0c9e75b6eebe57f0932b7cef11a6ba9c7
HenriqueBurle33 <henrique.ferreira@sou.inteli.edu.br>
2022-12-02 02:04:50 -0300

    commit

--- src/backend/routes/funcionarios.js
+++ src/backend/routes/funcionarios.js
@@ -7,7 +7,7 @@ const router = express.Router()
 app.post('/empreiteiras', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
-    let sql = `INSERT INTO empreiteiras (CNPJ, email, telefone, trabalhou_com_mrv, status_documentacao, id_endereco) VALUES ('${req.body.cnpj}', '${req.body.email}', '${req.body.celular}')`;
+    let sql = `INSERT INTO empreiteiras (CNPJ, email, telefone, ) VALUES ('${req.body.cnpj}', '${req.body.email}', '${req.body.celular}')`;
 
     // open the database
     let db = new sqlite3.Database(DBPATH);
