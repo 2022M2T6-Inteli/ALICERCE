fb800b81b97ffea9f3702804c18a29937d61e6be
HenriqueBurle33 <henrique.ferreira@sou.inteli.edu.br>
2022-12-01 21:01:19 -0300

    integracao teste

--- src/backend/routes/funcionarios.js
+++ src/backend/routes/funcionarios.js
@@ -7,7 +7,7 @@ const router = express.Router()
 app.post('/empreiteiras', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
-    let sql = `INSERT INTO empreiteiras (CNPJ, razao_social, nome_fantasia, quantidade_funcionarios, avaliacao, email, telefone, trabalhou_com_mrv, status_documentacao, id_endereco) VALUES ('${req.query.cnpj}', '${req.body.razao_social}', '${req.body.nome_fantasia}', '${req.body.quantidade_funcionarios}', '${req.body.avaliacao}', '${req.body.email}', '${req.body.trabalhou_com_mrv}', '${req.body.status_documentacao}', '${req.body.id_endereco}')`;
+    let sql = `INSERT INTO empreiteiras (CNPJ, email, telefone, trabalhou_com_mrv, status_documentacao, id_endereco) VALUES ('${req.body.cnpj}', '${req.body.email}', '${req.body.celular}')`;
 
     // open the database
     let db = new sqlite3.Database(DBPATH);
